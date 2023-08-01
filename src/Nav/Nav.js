ffaa215a16a8c2e13d45a08e0506e91702aa6ef5
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-08-01 11:15:39 -0600

    feat: Style the nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,9 +4,9 @@ import logo from '../materials/logo_header.png'
 const Nav = () => {
 
   return (
-    <>
-      <img src={logo} alt='ParkSeeker logo - green mountians set next to the word ParkSeeker'/>  
-    </>
+    <div className='nav'>
+      <img src={logo} className='header-img' alt='ParkSeeker logo - green mountians set next to the word ParkSeeker'/>  
+    </div>
   );
 }
 
