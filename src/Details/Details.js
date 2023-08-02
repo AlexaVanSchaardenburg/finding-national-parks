3baf398bbb14afc9c049d02c433ad455cd255d11
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-08-02 12:46:28 -0600

    feat: Build skeleton of details page

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,10 +1,55 @@
 import './Details.css';
 
-const Details = () => {
+const Details = ({ data }) => {
 
   return (
     <>
-      <h1>Details</h1>  
+      <div 
+        className='img' 
+        style={{
+          backgroundImage: `url(${data["images"][0]["url"]})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover'
+        }} >
+        <div className="park-summary-box">
+          <h1>Name Here</h1>
+          <p>States</p>
+          <p>Summary</p>
+        </div>
+      </div>
+      <div className='details'>
+        <h2 className='details-header'>at a glance</h2>
+          <div className='at-a-glance'>
+            <div className='info-box'>
+              <h3>activities</h3>
+              <p>all activites go here</p>
+            </div>
+            <div className='other-at-a-glance'>
+              <a href={data["url"]}>click here to go to park site!</a>
+              <div className='info-box'>
+                <h3>designation</h3>
+                <p>park designation here</p>
+              </div>
+              <div className='info-box'>
+                <h3>location</h3>
+                <p>park location here</p>
+              </div>
+            </div>
+          </div>
+        <h2 className='details-header'>more info</h2>
+          <div className='info-box'>
+            <h3>weather overview</h3>
+            <p>park weather overview here</p>
+            <h3>getting in</h3>
+            <p>park etrance fees here</p>
+          </div>
+        <h2 className='details-header'>park images</h2>
+          {data["images"].map(image => {
+            return (
+              <img src={image.url} alt={image.title}/>
+            )
+          })}
+      </div> 
     </>
   );
 }
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,12 +2,15 @@ import './Home.css';
 import Nav from "../Nav/Nav";
 import Form from "../Form/Form";
 import Card from "../Card/Card";
+//remove the below AND Details - this will all be passed in the route with the park code as the data and then the fetch will be excecuted with the parameters of the park code to display the data
+import Details from "../Details/Details";
 
 const Home = ({ data }) => {
 
   return (
     <>
       <Nav />
+      <Details data={ data.data[1] }/>
       <Form />
       <div className='cards'>
         {data.data.map(park => {
