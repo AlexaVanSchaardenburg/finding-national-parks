e670d8e19d9a7b355d62994dab5a30ab007ab908
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-08-02 17:28:56 -0600

    refactor: Updated the cards to be navlinks and fixed styling

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,17 +1,16 @@
 import './Card.css';
+import { NavLink } from 'react-router-dom'
 
 const Card = ({ park }) => {
 
   return (
-    <div className='card'>
-      <div className='img-box'>
-        <img src={park.images[0].url} className='img' alt='park image'/>
-      </div>
+    <NavLink className='card'>
+      <img src={park.images[0].url} className='img' alt='park image'/>
       <div className='name-and-activities'>
         <h2>{park.fullName}</h2>
         <p className='activities'>{`${park.activities[0].name} | ${park.activities[1].name} | see more...`}</p>
       </div>
-    </div>
+    </NavLink>
   );
 }
 
