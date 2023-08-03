8dd1f5570c9ffb4d639c21bdb9fa23737ff1afa6
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-08-02 22:05:46 -0600

    feat: Implement fetch request to display park names

--- src/App/App.js
+++ src/App/App.js
@@ -10,20 +10,27 @@ import mockData from '../mockData';
 
 const App = () => {
 
-  // const [allParks, setAllParks] = useState(null)
+  const [allParks, setAllParks] = useState(null)
 
   // useEffect(() => {
-  //   fetch(`https://developer.nps.gov/api/v1/parks?parkCode=&limit=471&start=0&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX`)
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       setAllParks(res)
-  //     })
-  //     .then(() => console.log(allParks))
-  //     .catch((error) => {
-  //       console.error('Error fetching data:', error);
-  //       setAllParks(null); 
-  //     });
+  //   console.log('mockData:', mockData)
+  //   setAllParks(mockData)
   // }, [])
+
+  // console.log(allParks)
+
+  useEffect(() => {
+    fetch(`https://developer.nps.gov/api/v1/parks?parkCode=&limit=471&start=0&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX`)
+      .then((res) => res.json())
+      .then((res) => {
+        setAllParks(res)
+      })
+      // .then(() => console.log(allParks))
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setAllParks(null); 
+      });
+  }, [])
 
   //pass all the parks to the cards - where it will map over and use the card component to display them
     //card component will need to be able to iterate over the activities of each park 
@@ -31,25 +38,63 @@ const App = () => {
   //when a park is clicked we need to get the id or short hand of that park then fetch jsut that park and pass that to the details component - usee the useParams in the url? put the shorthand for the park in the url, make details say loading until fetch returns with park details and it is displayed - look at rancid for example
 
   //need a state to store what the activity selected in the form - this state change will automatically trigger a function to filter through all the parks and pass those filtered parks to the the card component to display
-  
+
+  // const allParksView = (parks) => {
+  //   return (
+  //     <>
+  //       <Form />
+  //       <div className='cards'>
+  //         {parks.map(park => {
+  //           return <Card park={park}/>
+  //         })}
+  //       </div>
+  //     </>
+  //   )
+  // }
+
+  // const allParksView = (parks) => {
+  //   return (
+  //     <>
+  //       <Form />
+  //       <div className='cards'>
+  //         {parks.map(park => {
+  //           return <Card park={park} key={park.id} />
+  //         })}
+  //       </div>
+  //     </>
+  //   )
+  // }
+
   const allParksView = (parks) => {
     return (
       <>
         <Form />
         <div className='cards'>
           {parks.map(park => {
-            return <Card park={park}/>
+            return <Card park={park} key={park.id} />
           })}
         </div>
       </>
     )
-  }
+  };
+
+  // return (
+  //   <>
+  //     <Nav />
+  //     <Routes>
+  //       <Route path='/' element={<div>{allParksView(allParks.data)}</div>} />
+  //       <Route path='/:parkCode' element={<Details />} />
+  //     </Routes>
+  //   </>
+  // );
 
   return (
     <>
       <Nav />
-      <Routes >
-        <Route path='/' element={allParksView(mockData.data)}/>
+      <Routes>
+        {allParks && (
+          <Route path='/' element={<div>{allParksView(allParks.data)}</div>} />
+        )}
         <Route path='/:parkCode' element={<Details />} />
       </Routes>
     </>
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -5,10 +5,10 @@ const Card = ({ park }) => {
 
   return (
     <NavLink to={`/${park.parkCode}`} className='card'>
-      <img src={park.images[0].url} className='img' alt='park image'/>
+      {/* <img src={park.images[0].url} className='img' alt='park image'/> */}
       <div className='name-and-activities'>
         <h2>{park.fullName}</h2>
-        <p className='activities'>{`${park.activities[0].name} | ${park.activities[1].name} | see more...`}</p>
+        {/* <p className='activities'>{`${park.activities[0].name} | ${park.activities[1].name} | see more...`}</p> */}
       </div>
     </NavLink>
   );
