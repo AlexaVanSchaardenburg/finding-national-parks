34971561ada150be2126b34f19af2bf141a9f16a
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-08-02 20:43:33 -0600

    refactor: Removed the home component

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,29 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from 'react';
 import Home from "../Home/Home";
 import Details from "../Details/Details";
-import mockData from '../mockData'
+import Nav from "../Nav/Nav";
+import Form from "../Form/Form";
+import Card from "../Card/Card";
+import mockData from '../mockData';
 
 const App = () => {
 
-  //need a fetch request to get all the parks on page load
+  // const [allParks, setAllParks] = useState(null)
+
+  // useEffect(() => {
+  //   fetch(`https://developer.nps.gov/api/v1/parks?parkCode=&limit=471&start=0&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX`)
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       setAllParks(res)
+  //     })
+  //     .then(() => console.log(allParks))
+  //     .catch((error) => {
+  //       console.error('Error fetching data:', error);
+  //       setAllParks(null); 
+  //     });
+  // }, [])
 
   //pass all the parks to the cards - where it will map over and use the card component to display them
     //card component will need to be able to iterate over the activities of each park 
@@ -14,19 +31,29 @@ const App = () => {
   //when a park is clicked we need to get the id or short hand of that park then fetch jsut that park and pass that to the details component - usee the useParams in the url? put the shorthand for the park in the url, make details say loading until fetch returns with park details and it is displayed - look at rancid for example
 
   //need a state to store what the activity selected in the form - this state change will automatically trigger a function to filter through all the parks and pass those filtered parks to the the card component to display
+  
+  const allParksView = (parks) => {
+    return (
+      <>
+        <Form />
+        <div className='cards'>
+          {parks.map(park => {
+            return <Card park={park}/>
+          })}
+        </div>
+      </>
+    )
+  }
 
   return (
-    <Routes >
-      <Route path='/' element={<Home data={mockData}/>}/>
-      <Route path='/:parkCode' element={<Details />} />
-    </Routes>
+    <>
+      <Nav />
+      <Routes >
+        <Route path='/' element={allParksView(mockData.data)}/>
+        <Route path='/:parkCode' element={<Details />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
-
-  // useEffect(() => {
-  //   fetch('https://developer.nps.gov/api/v1/activities/parks?id=,&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX')
-  //     .then(res => res.json())
-  //     .then(res => console.log(res))
-  // },[]);
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,23 +1,40 @@
-import './Home.css';
-import Nav from "../Nav/Nav";
-import Form from "../Form/Form";
-import Card from "../Card/Card";
-//remove the below AND Details - this will all be passed in the route with the park code as the data and then the fetch will be excecuted with the parameters of the park code to display the data
-import Details from "../Details/Details";
+// import './Home.css';
+// import Nav from "../Nav/Nav";
+// import Form from "../Form/Form";
+// import Card from "../Card/Card";
+// import { useEffect, useState } from 'react'
 
-const Home = ({ data }) => {
+// const Home = () => {
 
-  return (
-    <>
-      <Nav />
-      <Form />
-      <div className='cards'>
-        {data.data.map(park => {
-          return <Card park={park}/>
-        })}
-      </div>
-    </>
-  );
-}
+//   const [allParks, setAllParks] = useState(null)
 
-export default Home;
+//   //need a fetch request to get all the parks on page load
+
+
+//   useEffect(() => {
+//     fetch(`https://developer.nps.gov/api/v1/parks?parkCode=&limit=471&start=0&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX`)
+//       .then((res) => res.json())
+//       .then((res) => {
+//         setAllParks(res)
+//       })
+//       .then(() => console.log(allParks))
+//       .catch((error) => {
+//         console.error('Error fetching data:', error);
+//         setAllParks(null); 
+//       });
+//   }, [])
+
+//   return (
+//     <>
+//       <Nav />
+//       <Form />
+//       <div className='cards'>
+//         {allParks.data.map(park => {
+//           return <Card park={park}/>
+//         })}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Home;
