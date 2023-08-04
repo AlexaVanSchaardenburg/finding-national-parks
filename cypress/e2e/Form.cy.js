80b377f676d475dd95759895045463d33c0bc973
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-08-04 15:38:21 -0600

    feat: Wrote the test for chekcing that the home view is what we expect on load

--- cypress/e2e/Form.cy.js
+++ cypress/e2e/Form.cy.js
@@ -1,10 +1,12 @@
 describe('User can filter parks by activity', () => {
-  it('should display parks that have rockclimbing', () => {
+  it.skip('should display parks that have rockclimbing', () => {
 
     //stub the fetch call
-    const parksFixture = 'parks.json'; 
-    cy.intercept('GET', 'https://developer.nps.gov/api/v1/parks?parkCode=&limit=471&start=0&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX', { fixture: parksFixture
+    const parksFixture = 'parks.json';
+    cy.intercept('GET', 'https://developer.nps.gov/api/v1/parks?parkCode=&limit=471&start=0&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX', {
+      fixture: parksFixture
     }).as('getParks');
+
     //visit the home page
     cy.visit('http://localhost:3000/');
     cy.wait('@getParks').its('response.statusCode').should('eq', 200);
@@ -21,11 +23,12 @@ describe('User can filter parks by activity', () => {
     cy.get('.cards').children().first().find('img').should('exist')
 
   })
-  it.only('should display a message when the user selects an activity that is not available at any park', () => {
+  it.skip('should display a message when the user selects an activity that is not available at any park', () => {
 
     //stub the fetch call
-    const parksFixture = 'parks.json'; 
-    cy.intercept('GET', 'https://developer.nps.gov/api/v1/parks?parkCode=&limit=471&start=0&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX', { fixture: parksFixture
+    const parksFixture = 'parks.json';
+    cy.intercept('GET', 'https://developer.nps.gov/api/v1/parks?parkCode=&limit=471&start=0&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX', {
+      fixture: parksFixture
     }).as('getParks');
 
     //visit the home page
@@ -38,4 +41,4 @@ describe('User can filter parks by activity', () => {
     //assert that the error message is displayed
     cy.get('p').should('have.text', 'there are no parks matching this activity, try looking for a different activity')
   })
-})
+});
--- cypress/e2e/Home.cy.js
+++ cypress/e2e/Home.cy.js
@@ -1,20 +1,44 @@
 describe('User can view all parks', () => {
   it('should dipslay all parks', () => {
-    cy.visit('https://example.cypress.io')
-    //stub fetch request for all parks
-    //visit site
+    //stub the fetch call
+    const parksFixture = 'parks.json'; 
+    cy.intercept('GET', 'https://developer.nps.gov/api/v1/parks?parkCode=&limit=471&start=0&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX', { fixture: parksFixture
+    }).as('getParks');
+
+    //visit the home page
+    cy.visit('http://localhost:3000/');
+    cy.wait('@getParks').its('response.statusCode').should('eq', 200);
+
+    //check that the form is visible to the user
+    cy.get('.form-box').should('be.visible')
+
     //check length of array of cards is what is expected
+    cy.get('.cards').children().should('have.length', 10);
+
     //check that the first card has an image, name, and list of expected activities
+    cy.get('.cards').children().first().find('h2').should('have.text', 'Antietam National Battlefield')
+    cy.get('.cards').children().first().find('.activities').should('have.text', 'Biking | Road Biking | Camping | see more...')
+    cy.get('.cards').children().first().find('img').should('exist')
   })
-  it('should display an error message if a 400 level error occurs', () => {
-    cy.visit('https://example.cypress.io')
-    //stub fetch request for all parks with 400 level error
+  it.skip('should display an error message if a 400 level error occurs', () => {
+    //stub the fetch call
+    const parksFixture = 'parks.json'; 
+    cy.intercept('GET', 'https://developer.nps.gov/api/v1/parks?parkCode=&limit=471&start=0&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX', { fixture: parksFixture
+    }).as('getParks');
+    //visit the home page
+    cy.visit('http://localhost:3000/');
+    cy.wait('@getParks').its('response.statusCode').should('eq', 200);
     //visit the site
     //assert that an error message is displayed
   })
-  it('should display an error message if a 500 level error occurspasses', () => {
-    cy.visit('https://example.cypress.io')
-      //stub fetch request for all parks with 500 level error
+  it.skip('should display an error message if a 500 level error occurspasses', () => {
+    //stub the fetch call
+    const parksFixture = 'parks.json'; 
+    cy.intercept('GET', 'https://developer.nps.gov/api/v1/parks?parkCode=&limit=471&start=0&api_key=l6jn2TRgOT3bXFR8Fk7iAF7OP6Bkf7lslJE9TMMX', { fixture: parksFixture
+    }).as('getParks');
+    //visit the home page
+    cy.visit('http://localhost:3000/');
+    cy.wait('@getParks').its('response.statusCode').should('eq', 200);
       //visit the site
       //assert that an error message is displayed
   })
