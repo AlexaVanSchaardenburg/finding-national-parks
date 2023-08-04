e1719b7e046ad129a128f85ea2088daf26c4e3fb
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-08-04 14:23:39 -0600

    feat: Configured the details spec with psuedocode

--- cypress/e2e/Details.cy.js
+++ cypress/e2e/Details.cy.js
@@ -1,5 +1,34 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('User can view all parks', () => {
+  it('should dipslay all parks', () => {
     cy.visit('https://example.cypress.io')
+    //stub fetch request for all parks
+    //stub fetch request for the specific park
+    //visit site
+    //click on the first card
+    //check that all of the following is visible on the page
+      //park name
+      //park state(s)
+      //park summary
+      //park activities list
+      //link to the parks site
+      //designation
+      //directions
+      //images - check that the number we expect to be there is visible
+  })
+  it('should display an error message if a 400 level error occurs', () => {
+    cy.visit('https://example.cypress.io')
+    //stub fetch request for all parks
+    //stub fetch request for the specific park with 400 level error
+    //visit the site
+    //click on the first card
+    //assert that an error message is displayed
+  })
+  it('should display an error message if a 500 level error occurspasses', () => {
+    cy.visit('https://example.cypress.io')
+    //stub fetch request for all parks
+    //stub fetch request for the specific park with 400 level error
+    //visit the site
+    //click on the first card
+    //assert that an error message is displayed
   })
 })
